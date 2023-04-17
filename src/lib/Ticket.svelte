d17bc79544918b4cd0fd47e34e74c5cc9fddedff
Alex Wilkins <86133577+calvirick@users.noreply.github.com>
2023-04-17 16:59:21 -0700

    Create Ticket Component

---
+++ src/lib/Ticket.svelte
@@ -0,0 +1,85 @@
+<script lang="ts">
+  import PillButton from '$lib/PillButton.svelte';
+</script>
+
+<div
+  class="floating | grid grid-flow-col auto-cols-max justify-items-center items-center"
+>
+  <div id="select" class="ticket-cell with-border |">
+    <input type="checkbox" class="select-checkbox" />
+  </div>
+  <div id="ticket_id" class="ticket-cell with-border | text-center">
+    <strong>12345</strong>
+  </div>
+  <div
+    id="ticket-title"
+    class="ticket-cell with-border | flex flex-row items-center h-[2.6rem] content-between"
+  >
+    <a href="/" class="w-[43.125em]">
+      Research Project - Video Templates for Agents
+    </a>
+    <div
+      id="status"
+      class="bg-violet-600 text-slate-50 rounded-full border border-solid border-black py-0 px-6 ml-10"
+    >
+      **New**
+    </div>
+  </div>
+  <div id="assigned_to" class="ticket-cell with-border |">
+    Assigned to: Alex W.
+  </div>
+  <div id="category" class="ticket-cell with-border | font-semibold italic">
+    AGENT REQUEST
+  </div>
+  <div
+    id="created_at"
+    class="ticket-cell with-border | font-semibold italic text-sm h-[2.6rem] justify-center items-center flex flex-row"
+  >
+    DATE CREATED: 4-24-23
+  </div>
+  <div id="priority" class="ticket-cell with-border | bg-sky-300">
+    <span
+      class="font-bold text-white bg-sky-500 border border-solid border-black rounded-full p-1 px-5"
+      >MEDIUM</span
+    >
+  </div>
+</div>
+
+<style lang="postcss">
+  :root {
+    --checkboxSize: 1.125rem;
+  }
+
+  .font-roboto-condensed {
+    font-family: 'Roboto Condensed', sans-serif;
+  }
+
+  input.select-checkbox {
+    position: relative;
+    top: 3px;
+    width: var(--checkboxSize);
+    height: var(--checkboxSize);
+  }
+
+  .ticket-cell {
+    @apply p-2 px-5;
+  }
+
+  .with-border {
+    @apply border border-solid border-black;
+  }
+
+  .floating {
+    @apply hover:bg-slate-200 hover:shadow-xl hover:transition-shadow;
+  }
+  .floating:hover {
+    position: relative;
+    bottom: 1px;
+  }
+
+  .floating:active {
+    position: relative;
+    bottom: 0px;
+    @apply ring-4;
+  }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,51 +1,72 @@
 <script lang="ts">
-  import PillButton from "$lib/PillButton.svelte";
+  import Ticket from '$lib/Ticket.svelte';
+
+  import PillButton from '$lib/PillButton.svelte';
 </script>
 
 <div id="new-ticket" class="p-5">
-  <PillButton href="/newticket" label="+ NEW TICKET" />
+  <PillButton href="#" label="+ NEW TICKET" />
 </div>
-
-<div class=" px-5 flex flex-row items-center justify-between">
-  <div class="font-roboto font-bold text-xl">OPEN TICKET</div>
-  <div class="font-roboto font-thin text-slate-500">
-    Number of Tickets: <strong>43</strong>
+<section id="tickets" class=" mx-auto">
+  <div class="  flex flex-row items-center justify-between">
+    <div class="font-roboto-condensed font-bold text-xl">OPEN TICKETS</div>
+    <div class="font-roboto-condensed font-thin text-slate-500">
+      Number of Tickets: <strong>43</strong>
+    </div>
   </div>
-</div>
-
-<table class="table-auto">
-  <thead class="border border-solid hover:bg-red-500">
-    <tr>
-      <th>Select</th>
-      <th>Ticket #</th>
-      <th>Title</th>
-      <th>Assigned To</th>
-      <th>Category</th>
-      <th>Date Created</th>
-      <th>Priority</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <td>The Sliding Mr. Bones (Next Stop, Pottersville)</td>
-      <td>Malcolm Lockyer</td>
-      <td>1961</td>
-    </tr>
-    <tr>
-      <td>Witchy Woman</td>
-      <td>The Eagles</td>
-      <td>1972</td>
-    </tr>
-    <tr>
-      <td>Shining Star</td>
-      <td>Earth, Wind, and Fire</td>
-      <td>1975</td>
-    </tr>
-  </tbody>
-</table>
+  <Ticket />
+  <Ticket />
+  <Ticket />
+  <Ticket />
+  <Ticket />
+  <Ticket />
+  <Ticket />
+  <Ticket />
+  <Ticket />
+  <Ticket />
+  <Ticket />
+  <Ticket />
+  <Ticket />
+  <Ticket />
+  <Ticket />
+  <Ticket />
+  <Ticket />
+  <Ticket />
+  <Ticket />
+  <Ticket />
+  <Ticket />
+  <Ticket />
+  <Ticket />
+  <Ticket />
+  <Ticket />
+  <Ticket />
+  <Ticket />
+  <Ticket />
+  <Ticket />
+  <Ticket />
+</section>
 
 <style lang="postcss">
-  .font-roboto {
-    font-family: "Roboto Condensed", sans-serif;
+  :root {
+    --checkboxSize: 1.125rem;
+  }
+
+  .font-roboto-condensed {
+    font-family: 'Roboto Condensed', sans-serif;
+  }
+
+  input.select-checkbox {
+    position: relative;
+    top: 3px;
+    width: var(--checkboxSize);
+    height: var(--checkboxSize);
+  }
+
+  .ticket-cell {
+    @apply p-2 px-5;
+  }
+
+  .with-border {
+    @apply border border-solid border-black;
   }
 </style>
