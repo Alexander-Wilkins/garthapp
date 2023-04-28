2e5c5244f7e5627a7b94ec190168d1952758254f
Alex Wilkins <86133577+calvirick@users.noreply.github.com>
2023-04-28 16:03:02 -0700

    begin styling the top containers

--- src/lib/utils/capitalize.ts
+++ src/lib/utils/capitalize.ts
@@ -1,3 +1,7 @@
 export function capitalizeFirstLetter(str: string): string {
   return str.charAt(0).toUpperCase() + str.slice(1);
 }
+
+export function capitalizeAllLetters(str: string): string {
+  return str.replace(/\b\w/g, (char) => char.toUpperCase());
+}
--- src/routes/ticket/[ticketId]/+page.svelte
+++ src/routes/ticket/[ticketId]/+page.svelte
@@ -1,13 +1,36 @@
 <script lang="ts">
   import { page } from '$app/stores';
   import ArrowButton from '$lib/ArrowButton.svelte';
+  import { capitalizeAllLetters } from '$lib/utils/capitalize';
 
   export let data: { title: string };
   const ticketId = $page.params.ticketId;
 </script>
 
-<ArrowButton href="/" />
-<h1 class="font-roboto-condensed text-xl font-extrabold">
-  #{ticketId}
-  {data.title}
-</h1>
+<section
+  id="grey-bg"
+  class="container mx-auto bg-neutral-200 rounded-lg border border-black px-6"
+>
+  <div id="ticket-title" class="container py-5">
+    <div id="back-wrapper" class="absolute left-[9rem]">
+      <ArrowButton href="/" />
+    </div>
+    <span class="font-roboto-condensed text-2xl font-extrabold">
+      #{ticketId}
+    </span>
+    <span class="text-2xl ml-0.5">{capitalizeAllLetters(data.title)}</span>
+  </div>
+  <div id="inner-container">
+    <div id="left-info-request-replies" class="container">
+      <section
+        id="top-agent-info-section"
+        class="container bg-white p-5 rounded-lg border border-black"
+      >
+        <button class="green-edit-button text-sm"
+          ><i class="fa-solid fa-pen" /> EDIT AGENT INFO</button
+        >
+        Agent Information: Templates: File Attachments:
+      </section>
+    </div>
+  </div>
+</section>
