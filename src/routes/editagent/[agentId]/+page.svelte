31db56def1b6d56d6822de3fd43098afe7cd8174
Alex Wilkins <86133577+calvirick@users.noreply.github.com>
2023-05-01 13:34:42 -0700

    Create edit Agent page route

---
+++ src/routes/editagent/[agentId]/+page.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  import { page } from '$app/stores';
+
+  import ArrowButton from '$lib/ArrowButton.svelte';
+
+  const agentId = $page.params.agentId;
+
+  console.log(agentId);
+</script>
+
+<ArrowButton href="/ticket/{agentId}" />
+
+would you like to freaking edit the agent?!?!? RELALYLYL?!?!?
--- src/routes/ticket/[ticketId]/+page.svelte
+++ src/routes/ticket/[ticketId]/+page.svelte
@@ -1,13 +1,16 @@
 <script lang="ts">
   import { toast } from '@zerodevx/svelte-toast';
   import { page } from '$app/stores';
+  import { capitalizeAllLetters } from '$lib/utils/capitalize';
+
   import ArrowButton from '$lib/ArrowButton.svelte';
   import Timer from '$lib/ticket/Timer.svelte';
-  import { capitalizeAllLetters } from '$lib/utils/capitalize';
 
   export let data: { title: string };
   const ticketId = $page.params.ticketId;
+  const agentId = ticketId;
 
+  // --------functions---------
   function copyAgentInfo() {
     const agentInfo = document.getElementById('agent-info') as HTMLElement;
     const agentInfoText = agentInfo.innerText;
@@ -17,6 +20,26 @@
 
   function copyToast() {
     toast.push('Agent Info Copied!', {
+      theme: {
+        '--toastBarHeight': 0,
+        '--toastColor': 'mintcream',
+        '--toastBackground': 'rgba(72,187,120,0.9)',
+        '--toastBarBackground': '#2F855A',
+      },
+    });
+  }
+  function downloadTemplateToast() {
+    toast.push('Downloaded Templates!', {
+      theme: {
+        '--toastBarHeight': 0,
+        '--toastColor': 'mintcream',
+        '--toastBackground': 'rgba(72,187,120,0.9)',
+        '--toastBarBackground': '#2F855A',
+      },
+    });
+  }
+  function downloadAttchmentToast() {
+    toast.push('Downloaded Attchments!', {
       theme: {
         '--toastBarHeight': 0,
         '--toastColor': 'mintcream',
@@ -47,9 +70,11 @@
         class="container bg-stone-100 p-5 rounded-lg border border-black"
       >
         <div id="agent-container" class="container flex flex-col">
-          <button class="green-edit-button text-sm"
-            ><i class="fa-solid fa-pen" /> EDIT AGENT INFO</button
-          >
+          <a href="/editagent/{agentId}">
+            <button class="green-edit-button text-sm"
+              ><i class="fa-solid fa-pen" /> EDIT AGENT INFO</button
+            >
+          </a>
           <div id="agent-content" class="flex flex-row gap-4 mt-3">
             <div id="agent-block" class="font-roboto-condensed font-bold">
               <div class="mb-2">AGENT INFORMATION:</div>
@@ -66,18 +91,65 @@
               </div>
               <button
                 id="copy-button"
-                class="tan-sm-button text-sm"
+                class="tan-sm-button text-sm font-sans"
                 on:click={copyAgentInfo}
-                ><i class="fa-solid fa-copy" /> Copy Info</button
+                ><i class="fa-solid fa-copy" /> Copy Agent Info</button
               >
             </div>
             <div class="vl" />
-            <div id="template-list" class="font-roboto-condensed font-bold">
-              TEMPLATES:
+            <div
+              id="templates"
+              class="font-roboto-condensed font-bold flex flex-col justify-between h-[226px]"
+            >
+              <div id="templates-list" class="flex flex-col">
+                <div class="mb-2">TEMPLATES:</div>
+                <span class="underline text-sky-500"
+                  ><i class="fa-solid fa-download" />
+                  <a href="#">MWC1-F</a></span
+                >
+                <span class="underline text-sky-500"
+                  ><i class="fa-solid fa-download" />
+                  <a href="#">GEN_LEGU1-F</a></span
+                >
+                <span class="underline text-sky-500"
+                  ><i class="fa-solid fa-download" />
+                  <a href="#">MWC4-F</a></span
+                >
+              </div>
+              <button
+                id="copy-button"
+                class="tan-sm-button text-sm font-sans"
+                on:click={downloadTemplateToast}
+                ><i class="fa-solid fa-download" /> All Templates</button
+              >
             </div>
             <div class="vl" />
-            <div id="file-attachments" class="font-roboto-condensed font-bold">
-              FILE ATTACHMENTS:
+            <div
+              id="templates"
+              class="font-roboto-condensed font-bold flex flex-col justify-between h-[226px]"
+            >
+              <div id="file-list" class="flex flex-col">
+                <div
+                  id="file-attachments"
+                  class="font-roboto-condensed font-bold"
+                >
+                  FILE ATTACHMENTS:
+                </div>
+                <span class="underline text-sky-500"
+                  ><i class="fa-solid fa-download" />
+                  <a href="#">profile-pic.png</a></span
+                >
+                <span class="underline text-sky-500"
+                  ><i class="fa-solid fa-download" />
+                  <a href="#">vanessa-ad.docx</a></span
+                >
+              </div>
+              <button
+                id="copy-button"
+                class="tan-sm-button text-sm font-sans"
+                on:click={downloadAttchmentToast}
+                ><i class="fa-solid fa-download" /> All Attachments</button
+              >
             </div>
           </div>
           <div
